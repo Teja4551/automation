7fd1b25eac7592148395b62a6c50e26832c79ef8
tejag31 <152838751+tejag31@users.noreply.github.com>
2024-02-16 11:11:13 +0530

    my first commit

---
+++ src/pract/Learn.java
@@ -0,0 +1,21 @@
+package pract;
+
+public class Learn {
+	
+	int x=10;
+	
+	public static void method1() {
+		
+		Learn obj=new Learn();
+		 
+		 System.out.println(obj.x);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		method1();
+
+	}
+
+}
